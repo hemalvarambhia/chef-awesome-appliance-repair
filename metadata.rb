7466ec683663f3d917af346c63ab46b93b07909a
Hemal <h.n.varambhia@gmail.com>
2015-01-29 15:03:26 +0000

    Begin with a recipe that does nothing

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-awesome-appliance-repair'
+maintainer       'Software Craftsman of London'
+maintainer_email 'hemal@softwarecraftsman.co.uk'
+license          'All rights reserved'
+description      'Configures server that hosts Awesome Appliance Repairs web application'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
