5fedd2db3d428a24075b2038f77ae30ec1a7e865
Hemal Varambhia <h.n.varambhia@gmail.com>
2015-01-30 11:09:08 +0000

    Apache service must be enabled and running

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -6,6 +6,11 @@ describe package("apache2") do
  it { should be_installed }
 end
 
+describe service("apache2") do
+ it { should be_enabled }
+ it { should be_running }
+end
+
 describe package("mysql-server") do
  it { should be_installed }
 end
