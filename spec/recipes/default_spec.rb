a8218eed654289394be9c9827ddb1f60c3c3a58c
Hemal Varambhia <h.n.varambhia@gmail.com>
2015-03-18 16:31:57 +0000

    Removed duplicate specs

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -13,30 +13,6 @@ describe "chef-awesome-appliance-repair::default" do
     it "installs and configures MySQL" do
       expect(chef_run).to include_recipe "chef-awesome-appliance-repair::database"
     end
-
-    it "installs MySQL server" do
-      expect(chef_run).to install_package "mysql-server"
-    end
-
-    it "installs the MySQL server client" do
-      expect(chef_run).to install_package "mysql-client"
-    end
-
-    it "copies over the application database creation script" do
-      expect(chef_run).to create_cookbook_file("make_AARdb.sql")
-    end
-
-    it "creates the AAR's relational database" do
-      expect(chef_run).to run_execute("create-AARdb").with(command: "mysql -u root < make_AARdb.sql")
-    end
-
-    it "creates the application database user" do
-      expect(chef_run).to run_execute("create-AARdb-user").with(command: "mysql -u root -e \"CREATE USER 'aarapp'@'localhost' IDENTIFIED BY '#{chef_run.node[:aar_db_password]}'\"")
-    end
-
-    it "grants aarapp SELECT, CREATE, DELETE and UPDATE privileges on AARdb" do
-      expect(chef_run).to run_execute("mysql -u root -e \"GRANT CREATE,INSERT,DELETE,UPDATE,SELECT on AARdb.* to aarapp@localhost\"")
-    end
   end
 
   describe "Application frameworks and tools" do
